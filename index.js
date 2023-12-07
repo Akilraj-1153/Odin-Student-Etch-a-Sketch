bb14019bbedd2764a68f5ee4b53b96deb6ceda47
AKILRAJ  N <122591298+Akilraj-1153@users.noreply.github.com>
2023-12-07 13:42:07 +0530

    added some functions in js

--- index.js
+++ index.js
@@ -12,24 +12,52 @@ document.addEventListener('DOMContentLoaded', function () {
     ipboxcount.setAttribute('min', 1);
     ipboxcount.setAttribute('max', 64);
     ipboxcount.setAttribute('step', 1);
+    ipboxcount.setAttribute('value', 4);
     ipgridrange.appendChild(ipboxcount);
 
     const value = document.querySelector("#value");
     const inputrange = document.querySelector("#iprange");
     value.textContent = inputrange.value;
     inputrange.addEventListener("input", (event) => {
-    value.textContent = event.target.value;
+        value.textContent = event.target.value;
     });
 
-    const newgame=document.getElementById('newgame');
-    newgame.addEventListener('click',()=>{
+    const newgame = document.getElementById('newgame');
+    newgame.addEventListener('click', () => {
         generategrid(ipboxcount.value);
     });
 
-    
+    let eraserMode = false;
+    let previousColor = '#000000'; 
+
+    const eraser = document.getElementById('eraser');
+    eraser.addEventListener('click', () => {
+        if (!eraserMode) {
+            previousColor = colorip.value;
+            colorip.value = '#ffffff';
+        } else {
+            colorip.value = previousColor;
+        }
+
+        eraserMode = !eraserMode;
+    });
+
+    const fulleraser = document.getElementById('fulleraser');
+    fulleraser.addEventListener('click', () => {
+        const cells = document.querySelectorAll('.smallbox');
+        cells.forEach(cell => {
+            cell.style.backgroundColor = 'white';
+        });
+    });
+
+    const reset = document.getElementById('Reset');
+    reset.addEventListener('click', () => {
+    location.reload();
+    });
 
     function generategrid(n) {
         Container.innerHTML = '';
+
         for (let i = 1; i <= n * n; i++) {
             const cell = document.createElement('div');
             cell.classList.add('smallbox');
@@ -48,9 +76,13 @@ document.addEventListener('DOMContentLoaded', function () {
             cell.style.boxSizing = 'border-box';
 
             cell.addEventListener('mouseover', () => {
-                cell.style.backgroundColor = colorip.value;
+                if (eraserMode) {
+                    cell.style.backgroundColor = 'white';
+                } else {
+                    cell.style.backgroundColor = colorip.value;
+                }
             });
         }
     }
+    generategrid(4);
 });
-
