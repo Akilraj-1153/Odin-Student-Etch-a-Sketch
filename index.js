245a77ed81560dd3b39bfe5c39cca01f108441df
AKILRAJ  N <122591298+Akilraj-1153@users.noreply.github.com>
2023-12-07 10:59:51 +0530

    added some functions in js and html

--- index.js
+++ index.js
@@ -1,8 +1,37 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const bigbox = document.querySelector('.bigbox');
 
-    for (let i = 1; i <= 4; i++) {
-        const div = document.createElement('div');
-        bigbox.appendChild(div);
+    const Container = document.getElementById('bigbox');
+    const Colorip = document.createElement('input');
+    const colorselect = document.getElementById('ipcolor');
+    Colorip.setAttribute('type', 'color');
+    colorselect.appendChild(Colorip);
+    const ipboxcount=document.createElement('input');
+    ipboxcount.setAttribute('type','range');
+    
+
+
+    for (let i = 1; i <= n * n; i++) {
+        const Cell = document.createElement('div');
+        Cell.classList.add('smallbox');
+        Container.appendChild(Cell);
+
+        const percentage = 100 / n;
+        Container.style.flex = `1 0 ${percentage}%`;
+        Container.style.maxHeight = '650px';
+        Container.style.maxWidth = '650px';
+        Container.style.flexWrap = 'wrap';
+
+        Cell.style.flex = `1 0 ${percentage}%`;
+        Cell.style.maxHeight = `${percentage}%`;
+        Cell.style.maxWidth = `${percentage}%`;
+        Cell.style.border = '1px solid black';
+        Cell.style.boxSizing = 'border-box';
+
+        Cell.addEventListener('mouseover', () => {
+            Cell.style.backgroundColor = Colorip.value;
+        });
     }
+
+   
 });
+var n = 2; 
